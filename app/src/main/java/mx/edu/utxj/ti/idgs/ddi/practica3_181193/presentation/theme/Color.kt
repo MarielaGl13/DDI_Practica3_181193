516bd14ff9f8ea7f5f607d1228d2de8c6e41fc53
Mariela Gonzalez Lopez <mery.glez03@gmail.com>
2023-05-22 11:48:19 -0500

    practica de wearables

---
+++ app/src/main/java/mx/edu/utxj/ti/idgs/ddi/practica3_181193/presentation/theme/Color.kt
@@ -0,0 +1,21 @@
+package mx.edu.utxj.ti.idgs.ddi.practica3_181193.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.wear.compose.material.Colors
+
+val Purple200 = Color(0xFFE91E63)
+val Purple500 = Color(0xFFE91E63)
+val Purple700 = Color(0xFFE91E63)
+val Teal200 = Color(0xFF03DAC5)
+val Red400 = Color(0xFFCF6679)
+
+internal val wearColorPalette: Colors = Colors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    secondaryVariant = Teal200,
+    error = Red400,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+    onError = Color.Black
+)
